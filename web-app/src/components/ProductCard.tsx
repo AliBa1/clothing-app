1413a0e4f87c972333d5b7d7db4cf65fd1aa1b8e
Ali <89438079+AliBa1@users.noreply.github.com>
2025-01-02 17:20:03 -0800

    underline on hover

--- web-app/src/components/ProductCard.tsx
+++ web-app/src/components/ProductCard.tsx
@@ -8,18 +8,18 @@ interface ProductCardProps {
 
 export default function ProductCard({ product }: ProductCardProps) {
   return (
-    <Link href={'/shop'} className='block rounded-3xl'>
+    <Link href={'/shop'} className='block rounded-3xl group'>
       <Image
         src={product.coverImg}
         alt={product.name}
         height={1280}
         width={1024}
+        loading='lazy'
         style={{ backgroundColor: 'white' }}
         className='aspect-[4/5] w-96 rounded object-cover object-center'
-        loading='lazy'
       />
 
-      <div className='py-2'>
+      <div className='py-2 group-hover:underline decoration-accent'>
         <h6>{product.name}</h6>
         <h6>${product.price}</h6>
       </div>
