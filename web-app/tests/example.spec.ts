bf62a69f5f6a0df07e0ec819f0f410f1c826b33d
Ali <89438079+AliBa1@users.noreply.github.com>
2025-01-16 22:10:40 -0800

    add filter tests + start feed page

--- web-app/tests/example.spec.ts
+++ web-app/tests/example.spec.ts
@@ -36,7 +36,22 @@ test.describe('navigation', () => {
   test('product click goes to product', async ({ page }) => {
     await page.getByRole('link', { name: 'Shop', exact: true }).click();
     await page.getByRole('link', { name: 'LIGHTWEIGHT BUCKLE PUFFER' }).click();
-    await expect(page).toHaveURL('/product/1');
+    await expect(page).toHaveURL(
+      '/product/alyx-lightweight-buckle-puffer-jacket/1'
+    );
+  });
+
+  test('brand click on shop goes to brand page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Shop', exact: true }).click();
+    await page.getByText('ALYX 9SM').click();
+    await expect(page).toHaveURL('/Alyx');
+  });
+
+  test('brand click on product page goes to brand page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Shop', exact: true }).click();
+    await page.getByRole('link', { name: 'LIGHTWEIGHT BUCKLE PUFFER' }).click();
+    await page.getByRole('img', { name: 'ALYX 9SM' }).click();
+    await expect(page).toHaveURL('/Alyx');
   });
 
   test('has header', async ({ page }) => {
@@ -66,9 +81,7 @@ test.describe('size', () => {
     page
   }) => {
     await page.getByRole('link', { name: 'Shop', exact: true }).click();
-    await page
-      .getByRole('link', { name: 'STMT Hoodie $58 $' })
-      .click();
+    await page.getByRole('link', { name: 'STMT Hoodie $58 $' }).click();
     await page.getByRole('button', { name: 'XL', exact: true }).click();
     await page.getByRole('img', { name: 'Grey' }).click();
 
@@ -90,5 +103,294 @@ test.describe('size', () => {
     const selectedSizeM = await page.locator('p:text("Size ·")');
     await expect(selectedSizeM).toBeVisible();
     await expect(selectedSizeM).toHaveText('Size · M');
+  });
+});
+
+test.describe('color', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('select color changes color', async ({ page }) => {
+    await page.getByRole('link', { name: 'Shop', exact: true }).click();
+    await page.getByRole('link', { name: 'STMT Hoodie $58 $' }).click();
+
+    await page.getByRole('img', { name: 'Grey' }).click();
+
+    const selectedColorText = await page.locator('p:text("Color ·")');
+    await expect(selectedColorText).toBeVisible();
+    await expect(selectedColorText).toHaveText('Color · Grey');
+  });
+});
+
+test.describe('filters', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('filters modal opens, sees all options, then applies them', async ({
+    page
+  }) => {
+    await page.getByRole('link', { name: 'Shop', exact: true }).click();
+    await expect(page.getByRole('button', { name: 'Filters' })).toBeVisible();
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - button "Close Modal":
+        - img
+      - heading "Filters" [level=4]
+      - 'button "Sort: Default"':
+        - paragraph: "Sort: Default"
+      - group: "Default New Popular Price: Low to High Price: High to Low"
+      - 'button "Gender: Any"':
+        - paragraph: "Gender: Any"
+      - group: Any Men Women Unisex Kids
+      - 'button "Inventory: Any"':
+        - paragraph: "Inventory: Any"
+      - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+      - button "Fit":
+        - paragraph: Fit
+      - group:
+        - checkbox "Boxy": boxy
+        - text: Boxy
+        - checkbox "Oversized": oversized
+        - text: Oversized
+        - checkbox "Regular": regular
+        - text: Regular
+        - checkbox "Relaxed": relaxed
+        - text: Relaxed
+        - checkbox "Skinny": skinny
+        - text: Skinny
+        - checkbox "Slim": slim
+        - text: Slim
+      - button "Clear All"
+      - button "Apply"
+      `);
+
+    await expect(page.locator('form')).toContainText('Sort: Default');
+    await page.getByRole('button', { name: 'Sort: Default' }).click();
+    await expect(page.locator('label:text("Popular")')).toBeVisible();
+    await page.locator('label').filter({ hasText: 'Popular' }).click();
+    // await page.waitForTimeout(500);
+    await expect(
+      page.locator('button').filter({ hasText: 'Sort: Popular' })
+    ).toBeVisible();
+    // await expect(page.locator('label').filter({ hasText: 'Default' })).toBeHidden();
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - button "Close Modal":
+        - img
+      - heading "Filters" [level=4]
+      - 'button "Sort: Popular"':
+        - paragraph: "Sort: Popular"
+      - group: "Default New Popular Price: Low to High Price: High to Low"
+      - 'button "Gender: Any"':
+        - paragraph: "Gender: Any"
+      - group: Any Men Women Unisex Kids
+      - 'button "Inventory: Any"':
+        - paragraph: "Inventory: Any"
+      - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+      - button "Fit":
+        - paragraph: Fit
+      - group:
+        - checkbox "Boxy": boxy
+        - text: Boxy
+        - checkbox "Oversized": oversized
+        - text: Oversized
+        - checkbox "Regular": regular
+        - text: Regular
+        - checkbox "Relaxed": relaxed
+        - text: Relaxed
+        - checkbox "Skinny": skinny
+        - text: Skinny
+        - checkbox "Slim": slim
+        - text: Slim
+      - button "Clear All"
+      - button "Apply"
+      `);
+
+    await page.getByRole('button', { name: 'Gender: Any' }).click();
+    // await expect(page.locator('label:text("Men")')).toBeVisible();
+    await page.getByText('Men', { exact: true }).click();
+    // await page.waitForTimeout(500);
+    await expect(page.locator('form')).toContainText('Gender: Men');
+    await expect(
+      page.locator('button').filter({ hasText: 'Gender: Men' })
+    ).toBeVisible();
+    // await expect(page.locator('label:text("Men")')).toBeHidden();
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - button "Close Modal":
+        - img
+      - heading "Filters" [level=4]
+      - 'button "Sort: Popular"':
+        - paragraph: "Sort: Popular"
+      - group: "Default New Popular Price: Low to High Price: High to Low"
+      - 'button "Gender: Men"':
+        - paragraph: "Gender: Men"
+      - group: Any Men Women Unisex Kids
+      - 'button "Inventory: Any"':
+        - paragraph: "Inventory: Any"
+      - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+      - button "Fit":
+        - paragraph: Fit
+      - group:
+        - checkbox "Boxy": boxy
+        - text: Boxy
+        - checkbox "Oversized": oversized
+        - text: Oversized
+        - checkbox "Regular": regular
+        - text: Regular
+        - checkbox "Relaxed": relaxed
+        - text: Relaxed
+        - checkbox "Skinny": skinny
+        - text: Skinny
+        - checkbox "Slim": slim
+        - text: Slim
+      - button "Clear All"
+      - button "Apply"
+      `);
+
+    await page.getByRole('button', { name: 'Inventory: Any' }).click();
+    // await expect(page.locator('label:text("Pre-Order")')).toBeVisible();
+    await page.getByText('Pre-Order').click();
+    // await page.waitForTimeout(500);
+    await expect(
+      page.locator('button').filter({ hasText: 'Inventory: Pre-Order' })
+    ).toBeVisible();
+    // await expect(page.locator('label:text("Pre-Order")')).toBeHidden();
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - button "Close Modal":
+        - img
+      - heading "Filters" [level=4]
+      - 'button "Sort: Popular"':
+        - paragraph: "Sort: Popular"
+      - group: "Default New Popular Price: Low to High Price: High to Low"
+      - 'button "Gender: Men"':
+        - paragraph: "Gender: Men"
+      - group: Any Men Women Unisex Kids
+      - 'button "Inventory: Pre-Order"':
+        - paragraph: "Inventory: Pre-Order"
+      - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+      - button "Fit":
+        - paragraph: Fit
+      - group:
+        - checkbox "Boxy": boxy
+        - text: Boxy
+        - checkbox "Oversized": oversized
+        - text: Oversized
+        - checkbox "Regular": regular
+        - text: Regular
+        - checkbox "Relaxed": relaxed
+        - text: Relaxed
+        - checkbox "Skinny": skinny
+        - text: Skinny
+        - checkbox "Slim": slim
+        - text: Slim
+      - button "Clear All"
+      - button "Apply"
+      `);
+
+    await page.getByRole('button', { name: 'Fit' }).click();
+    // await expect(page.locator('label:text("Boxy")')).toBeVisible();
+    // await page.locator('label').filter({ hasText: 'Boxy' }).click();
+    await page.getByText('Boxy').click();
+    // await page.waitForTimeout(500);
+    await expect(page.getByLabel('Boxy')).toBeChecked();
+    await expect(
+      page.locator('div').filter({ hasText: /^Boxy$/ })
+    ).toBeVisible();
+    await expect(
+      page.locator('button').filter({ hasText: 'Fit: Boxy' })
+    ).toBeVisible();
+    // await expect(page.locator('label:text("Boxy")')).toBeHidden();
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+      - button "Close Modal":
+        - img
+      - heading "Filters" [level=4]
+      - 'button "Sort: Popular"':
+        - paragraph: "Sort: Popular"
+      - group: "Default New Popular Price: Low to High Price: High to Low"
+      - 'button "Gender: Men"':
+        - paragraph: "Gender: Men"
+      - group: Any Men Women Unisex Kids
+      - 'button "Inventory: Pre-Order"':
+        - paragraph: "Inventory: Pre-Order"
+      - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+      - 'button "Fit: Boxy"':
+        - paragraph: "Fit: Boxy"
+      - group:
+        - checkbox "Boxy" [checked]: boxy
+        - text: Boxy
+        - checkbox "Oversized": oversized
+        - text: Oversized
+        - checkbox "Regular": regular
+        - text: Regular
+        - checkbox "Relaxed": relaxed
+        - text: Relaxed
+        - checkbox "Skinny": skinny
+        - text: Skinny
+        - checkbox "Slim": slim
+        - text: Slim
+      - button "Clear All"
+      - button "Apply"
+      `);
+
+    await page.getByRole('button', { name: 'Apply' }).click();
+    await page.waitForTimeout(500);
+    await expect(page).toHaveURL(
+      '/shop?sort=popular&gender=men&inventory=preOrder&fit=boxy'
+    );
+  });
+
+  test('clear all resets to default', async ({ page }) => {
+    await page.goto('http://localhost:3000/shop');
+    await page.getByRole('button', { name: 'Filters' }).click();
+    await page.getByRole('button', { name: 'Sort: Default' }).click();
+    await page.getByRole('dialog').getByText('Popular').click();
+    await page.getByRole('button', { name: 'Gender: Any' }).click();
+    await page.getByText('Unisex').click();
+    await page.getByRole('button', { name: 'Inventory: Any' }).click();
+    await page.getByText('Ready to Ship').click();
+    await page.getByRole('button', { name: 'Fit' }).click();
+    await page.getByLabel('Boxy').check();
+    await page.getByLabel('Oversized').check();
+    await page.getByLabel('Regular').check();
+    await page
+      .getByRole('button', { name: 'Fit: Boxy, Oversized, Regular' })
+      .click();
+    await page.getByRole('button', { name: 'Clear All' }).click();
+    await expect(page.locator('form')).toContainText('Sort: Default');
+    await expect(page.locator('form')).toContainText('Gender: Any');
+    await expect(page.locator('form')).toContainText('Inventory: Any');
+    await expect(page.locator('form')).toContainText('Fit');
+    await expect(page.getByRole('dialog')).toMatchAriaSnapshot(`
+    - button "Close Modal":
+      - img
+    - heading "Filters" [level=4]
+    - 'button "Sort: Default"':
+      - paragraph: "Sort: Default"
+    - group: "Default New Popular Price: Low to High Price: High to Low"
+    - 'button "Gender: Any"':
+      - paragraph: "Gender: Any"
+    - group: Any Men Women Unisex Kids
+    - 'button "Inventory: Any"':
+      - paragraph: "Inventory: Any"
+    - group: Any Ready to Ship Pre-Order Low Stock Out of Stock
+    - button "Fit":
+      - paragraph: Fit
+    - group:
+      - checkbox "Boxy": boxy
+      - text: Boxy
+      - checkbox "Oversized": oversized
+      - text: Oversized
+      - checkbox "Regular": regular
+      - text: Regular
+      - checkbox "Relaxed": relaxed
+      - text: Relaxed
+      - checkbox "Skinny": skinny
+      - text: Skinny
+      - checkbox "Slim": slim
+      - text: Slim
+    - button "Clear All"
+    - button "Apply"
+    `);
   });
 });
