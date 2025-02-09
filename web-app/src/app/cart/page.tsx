5db9f5af3f730b6858b3f1d8cbb12772a291e54b
Ali <89438079+AliBa1@users.noreply.github.com>
2025-02-08 16:16:27 -0800

    complete cart page + add more colors

--- web-app/src/app/cart/page.tsx
+++ web-app/src/app/cart/page.tsx
@@ -1,25 +1,54 @@
 import CartProductCard from '@/components/CartProductCard';
 import { cartProducts } from '@/interfaces/userProducts';
+import Link from 'next/link';
 
 export default function CartPage() {
+  const cartEmpty = false;
   return (
     <main>
-      <h1>Cart</h1>
-      <div className='flex justify-between'>
-        <div className='flex flex-col gap-8'>
-          {cartProducts.map((p) => (
-            <CartProductCard
-              key={`${p.product.id}-${p.color.id}`}
-              cartProduct={p}
-            />
-          ))}
+      {cartEmpty ? (
+        <div className='mt-4 flex flex-col items-center'>
+          <h4>Your cart is empty</h4>
+          <Link href={'/shop'} className='btn-primary py-4 rounded-full'>Shop</Link>
         </div>
+      ) : (
+        <main className='md:flex-row items-start justify-center'>
+          <div
+            className='flex flex-col gap-4 rounded p-2'
+            style={{ scrollbarWidth: 'none' }}
+          >
+            <h4>Cart</h4>
+            {cartProducts.map((p) => (
+              <CartProductCard
+                key={`${p.product.id}-${p.color.id}`}
+                cartProduct={p}
+              />
+            ))}
+          </div>
 
-        <div className='sticky top-16 right-0'>
-          <p>Total: $123.12</p>
-          <button className='btn-primary'>Checkout</button>
-        </div>
-      </div>
+          <div className='sticky bottom-0 w-full md:w-auto bg-background md:bg-auto md:top-16 right-0 rounded text-base md:text-xl p-2 md:p-4 flex flex-col'>
+            <div className='flex justify-between'>
+              <p>Subtotal</p>
+              <p>$124.56</p>
+            </div>
+            {/* <div className='flex justify-between'>
+            <p>Discount</p>
+            <p>$124.56</p>
+          </div> */}
+            <p className='italic text-sm md:text-base'>
+              Shipping and taxes calculated at checkout
+            </p>
+            <div className='flex flex-col gap-4 mt-4'>
+              <button className='btn-primary py-4 rounded-full'>
+                Checkout
+              </button>
+              <button className='btn-primary bg-error rounded-full'>
+                Empty Cart
+              </button>
+            </div>
+          </div>
+        </main>
+      )}
     </main>
   );
 }
