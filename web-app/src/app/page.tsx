11bbda2bb50f600da994714e6d71ce97055bb18f
Ali <89438079+AliBa1@users.noreply.github.com>
2025-02-09 21:34:44 -0800

    draft banner wide

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link';
+import ProductBannerWide from '@/components/ProductBannerWide';
+import { mockProducts } from '@/interfaces/brandProducts';
 
 export default function Home() {
   return (
     <main>
-      <Link href={'/shop'}>Click to Shop</Link>
+      <ProductBannerWide product={mockProducts[0]} />
+      <ProductBannerWide product={mockProducts[1]} colorVariant={mockProducts[1].colors[2]} />
     </main>
   );
 }
--- web-app/src/components/ProductBannerWide.tsx
+++ web-app/src/components/ProductBannerWide.tsx
@@ -1,7 +1,70 @@
-export default function ProductBannerWide() {
+import { BrandProduct, ColorVariant } from '@/interfaces/brandProducts';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface BannerProps {
+  product: BrandProduct;
+  colorVariant?: ColorVariant;
+}
+
+export default function ProductBannerWide({
+  product,
+  colorVariant
+}: BannerProps) {
+  const image: string =
+    colorVariant?.images.cover || product.colors[0].images.cover;
+  const color: string = colorVariant?.colorName || product.colors[0].colorName;
   return (
-    <div>
-      <p>Product Banner</p>
+    <div className='w-full flex justify-between px-8 bg-white'>
+      <div className='flex flex-col justify-evenly text-secondary'>
+        <h4>{product.name}</h4>
+        <div className='flex gap-4'>
+          <Link href={`/product/${product.productSlug}/${product.id}?color=${color}`} className='btn-accent'>
+            View Item
+          </Link>
+          <Link
+            href={`/${product.brand.handle}`}
+            className='btn-secondary border-secondary'
+          >
+            Shop {product.brand.name}
+          </Link>
+        </div>
+      </div>
+
+      <div className='mx-auto flex flex-col items-center'>
+        <Image
+          // src={product.colors[0].images.cover}
+          src={image}
+          alt={`'${color}' ${product.name}`}
+          height={1280 / 2}
+          width={1024 / 2}
+          className='aspect-[4/5] w-full rounded object-cover object-center'
+          // className='aspect-[4/5] w-full rounded object-cover object-center bg-background dark:bg-white'
+          priority
+        />
+
+        {/* {product?.colors.length > 1 && (
+          <div className='hidden md:flex gap-1 justify-center'>
+            {product.colors.map((c) => (
+              <button
+                key={`${product.name}-${c.colorName}`}
+                className='btn-circle h-8 w-8'
+                style={{
+                  background: `${
+                    c.primaryColor.broadColor === 'multicolor'
+                      ? 'conic-gradient(red, yellow, green, blue, purple)'
+                      : `rgb(${c.primaryColor.red}, ${c.primaryColor.green}, ${c.primaryColor.blue})`
+                  }`
+                }}
+                onClick={() => {
+                  setImage(c.images.cover);
+                  setColor(c.colorName);
+                }}
+              />
+            ))}
+          </div>
+        )} */}
+      </div>
     </div>
   );
 }
