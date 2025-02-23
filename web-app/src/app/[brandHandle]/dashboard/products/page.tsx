759bf00ac64f7a260c36200a215601dea3df3ab1
Ali <89438079+AliBa1@users.noreply.github.com>
2025-02-23 12:52:18 -0800

    improve products table

--- web-app/src/app/[brandHandle]/dashboard/products/page.tsx
+++ web-app/src/app/[brandHandle]/dashboard/products/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { mockProducts } from '@/interfaces/brandProducts';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 export default function DashboardProductsPage() {
@@ -20,12 +21,13 @@ export default function DashboardProductsPage() {
         <thead className='bg-slate-600'>
           <tr>
             <th className='text-start px-4 py-2'>ID</th>
+            <th className='text-start px-4 py-2'>Image</th>
             <th className='text-start px-4 py-2'>Name</th>
-            <th className='text-start px-4 py-2'>Categories</th>
-            <th className='text-start px-4 py-2'>SubCategories</th>
-            <th className='text-start px-4 py-2'>Types</th>
-            <th className='text-start px-4 py-2'>Color</th>
             <th className='text-start px-4 py-2'>Price</th>
+            <th className='text-start px-4 py-2'>Color</th>
+            <th className='text-start px-4 py-2'>Category</th>
+            <th className='text-start px-4 py-2'>Subcategory</th>
+            {/* <th className='text-start px-4 py-2'>Types</th> */}
             <th className='text-start px-4 py-2'>Discount</th>
           </tr>
         </thead>
@@ -34,12 +36,26 @@ export default function DashboardProductsPage() {
             p.colors.map((cV) => (
               <tr key={cV.id} className='border-b last:border-none'>
                 <td className='px-4 py-2'>{cV.id}</td>
+                <td className='px-4 py-2'>
+                  <Image
+                    src={cV.images.cover}
+                    alt={`${p.name} Cover Image`}
+                    height={1280 / 8}
+                    width={1024 / 8}
+                    className='aspect-[4/5] rounded object-cover object-center bg-background dark:bg-white'
+                    loading='lazy'
+                  />
+                </td>
                 <td className='px-4 py-2'>{p.name}</td>
-                <td className='px-4 py-2'>{p.categories.map((c) => c.label).join(', ')}</td>
-                <td className='px-4 py-2'>{p.subCategories.map((sC) => sC.label).join(', ')}</td>
-                <td className='px-4 py-2'>{p.types.map((t) => t.label).join(', ')}</td>
-                <td className='px-4 py-2'>{cV.colorName}</td>
                 <td className='px-4 py-2'>${cV.price}</td>
+                <td className='px-4 py-2'>{cV.colorName}</td>
+                <td className='px-4 py-2'>
+                  {p.categories.map((c) => c.label).join(', ')}
+                </td>
+                <td className='px-4 py-2'>
+                  {p.subCategories.map((sC) => sC.label).join(', ')}
+                </td>
+                {/* <td className='px-4 py-2'>{p.types.map((t) => t.label).join(', ')}</td> */}
                 <td className='px-4 py-2'>
                   {cV.discount
                     ? cV.discount.type === 'percent'
